55c88a959f083aa07dc409a5d914ebc397b3ea77
Thiago Garcia <tgalves18@gmail.com>
2018-08-20 23:39:25 -0300

    Base project with tableview set up

---
+++ Todoey/TodoListViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Thiago Garcia on 20/08/18.
+//  Copyright © 2018 iGarcia. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+    
+    let itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    
+    //MARK - Tableview Datasource Methods
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "toDoItemCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    }
+}
+
